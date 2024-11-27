f510c33dd89356f52b0f914f8f765826e77446b8
dyedi <jmcantalejoperez@gmail.com>
2024-11-27 13:09:26 +0100

    1

--- src/Programacion/Ejercicio_1.java
+++ src/Programacion/Ejercicio_1.java
@@ -1,5 +1,22 @@
 package Programacion;
 
-public class Ejercicio_1 {
+import java.util.Scanner;
 
+public class Ejercicio_1 {
+	public static void main(String[] args) {
+//		Realizar un programa que haga lo siguiente:
+//			1. Mostrar un mensaje pidiendo el nombre del usuario
+//			2. Esperar a que el usuario escriba su nombre y registrarlo en una variable
+//			3. Mostrar un nuevo mensaje pidiendo el apellido del usuario
+//			4. Esperar a que el usuario escriba su apellido y registrarlo en otra variable
+//			5. Mostrar un mensaje final indicando: "Su nombre completo es ... (nombre y apellido)"
+
+		Scanner terminator = new Scanner(System.in);
+		System.out.println("Dame tu nombre: ");
+		String nombre = terminator.nextLine();
+		System.out.println("Dame tu apellido: ");
+		String apellido = terminator.nextLine();
+		System.out.println("Tu nombre completo es " + nombre + " " + apellido);
+		terminator.close();
+	}
 }
---
+++ src/Programacion/Ejercicio_2.java
@@ -0,0 +1,28 @@
+package Programacion;
+
+import java.util.Scanner;
+
+public class Ejercicio_2 {
+	public static void main(String[] args) {
+//		Realizar un programa que trabaje del siguiente modo:
+//			1. Mostrar un mensaje pidiendo el nombre del usuario
+//			2. Esperar a que el usuario escriba su nombre
+//			3. Mostrar un nuevo mensaje indicando: “Bienvenido (nombre)”
+//			4. Mostrar un nuevo mensaje indicando: “¿De dónde eres?”
+//			5. Esperar a que el usuario escriba el lugar de donde vive
+//			6. Mostrar un mensaje preguntando: “¿Qué tal se vive en (localidad)?”
+//			7. Esperar a que el usuario escriba una respuesta
+//			8. Mostrar un último mensaje indicando “Gracias. Un saludo”
+		
+		Scanner terminator = new Scanner(System.in);
+		System.out.println("Dame tu nombre: ");
+		String nombre = terminator.nextLine();
+		System.out.println("Bienvenido " + nombre);
+		System.out.println("De donde eres?: ");
+		String localidad = terminator.nextLine();
+		System.out.println("Que tal se vive en " + localidad + " ?");
+		terminator.nextLine();
+		System.out.println("Me alegro, gracias");
+		
+	}
+}
